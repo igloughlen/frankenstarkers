cc89aa8fd8d42b7c5c6844c1a4174489b635ed25
sebastianschmid <sebastian.schmid@100und1.com>
2013-04-09 17:34:19 +0300

    Update starkers-utilities.php
    removed removal of body class "blog"

--- external/starkers-utilities.php
+++ external/starkers-utilities.php
@@ -72,12 +72,7 @@
     	public static function add_slug_to_body_class( $classes ) {
     		global $post;
 	   
-    		if( is_home() ) {			
-    			$key = array_search( 'blog', $classes );
-    			if($key > -1) {
-    				unset( $classes[$key] );
-    			};
-    		} elseif( is_page() ) {
+    		if( is_page() ) {
     			$classes[] = sanitize_html_class( $post->post_name );
     		} elseif(is_singular()) {
     			$classes[] = sanitize_html_class( $post->post_name );

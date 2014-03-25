e0d8e1a0b014c37c2cf8c7207ab50bf0ededef83
igloughlen <igloughlen@gmail.com>
2014-03-24 22:53:31 -0700

    Revert afc7d24..21d4ea8
    This rolls back to commit afc7d24ab4738acbe74786370a3e95e930a57984.

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,9 +1,4 @@
-	<!-- jQuery
-    ================================================== -->
-    <!-- Remove if you don't plan on using jQuery --> 
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-
-    <!-- Bootstrap javascript
+	<!-- Bootstrap javascript
     ================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->    
     <script src="<?php echo get_template_directory_uri();?>/js/jquery.js"></script>
@@ -19,18 +14,6 @@
     <script src="<?php echo get_template_directory_uri();?>/js/bootstrap-collapse.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/bootstrap-carousel.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/bootstrap-typeahead.js"></script>
-
-    <!--Responsive-Nav Hookup-->
-    <script>
-      var nav = responsiveNav(".nav-collapse");
-    </script>
-
-    <?php
-    /* Always have wp_footer() just before the closing </body>
-     * tag of your theme, or you will break many plugins, which
-     * generally use this hook to reference JavaScript files.
-     */
-    wp_footer();
-    ?>
+	<?php wp_footer(); ?>
 	</body>
 </html>

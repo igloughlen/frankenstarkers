04d6ea2880c86f2a4f02494deed98e01eefded25
igloughlen <igloughlen@gmail.com>
2014-03-24 20:02:57 -0700

    Add container div to header, add markup to description

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,6 @@
-<header>
-	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php bloginfo( 'description' ); ?>
-	<?php get_search_form(); ?>
-</header>
+<div class="container">
+	<header>
+		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<h3><?php bloginfo( 'description' ); ?></h3>
+		<?php get_search_form(); ?>
+	</header>

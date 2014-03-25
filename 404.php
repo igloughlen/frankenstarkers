e645742fee4026641ddb3cb5444ee07f3948e40c
igloughlen <igloughlen@gmail.com>
2014-03-25 16:08:42 -0700

    Add search to 404 page

--- 404.php
+++ 404.php
@@ -12,5 +12,6 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <h2>Page not found</h2>
+<div id="found_search"><?php get_search_form(); ?></div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>

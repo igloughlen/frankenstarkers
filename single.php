d238fb842c5cc2b110020481be32b2cce0c3ee2c
igloughlen <igloughlen@gmail.com>
2014-03-25 21:29:06 -0700

    Add post navigation to single.php

--- single.php
+++ single.php
@@ -24,6 +24,55 @@
 	<h3>About <?php echo get_the_author() ; ?></h3>
 	<?php the_author_meta( 'description' ); ?>
 	<?php endif; ?>
+		<div id="post-nav" class="row">
+			<div id="prev-post">
+			    <?php $prevPost = get_previous_post(true);
+			        if($prevPost) {
+			            $args = array(
+			                'posts_per_page' => 1,
+			                'include' => $prevPost->ID
+			            );
+			            $prevPost = get_posts($args);
+			            foreach ($prevPost as $post) {
+			                setup_postdata($post);
+			    ?>
+			        <div class="post-previous">
+			            <a class="previous" href="<?php the_permalink(); ?>">&laquo; Previous Story</a>
+			            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+			            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+			            <small><?php the_date('F j, Y'); ?></small>
+			        </div>
+			    <?php
+			                wp_reset_postdata();
+			            } //end foreach
+			        } // end if
+			    ?>
+			</div><!-- #prev-post -->
+			<div id="next-post">
+			    <?php
+			        $nextPost = get_next_post(true);
+			        if($nextPost) {
+			            $args = array(
+			                'posts_per_page' => 1,
+			                'include' => $nextPost->ID
+			            );
+			            $nextPost = get_posts($args);
+			            foreach ($nextPost as $post) {
+			                setup_postdata($post);
+			    ?>
+			        <div class="post-next">
+			            <a class="next" href="<?php the_permalink(); ?>">Next Story &raquo;</a>
+			            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+			            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+			            <small><?php the_date('F j, Y'); ?></small>
+			        </div>
+			    <?php
+			                wp_reset_postdata();
+			            } //end foreach
+			        } // end if
+			    ?>
+			</div><!-- #next-post -->
+		</div><!-- #post-nav -->
 
 	<?php comments_template( '', true ); ?>
 

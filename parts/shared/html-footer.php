bb113d1d05bfdfd3a1c25f51b855276232c2376e
igloughlen <igloughlen@gmail.com>
2014-03-27 02:28:06 -0700

    Delete bootstrap elements

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,22 +1,11 @@
-	<!-- jQuery
-    ================================================== -->
-    <!-- Remove if you don't plan on using jQuery --> 
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-
     <!-- Bootstrap javascript
     ================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->    
     <script src="<?php echo get_template_directory_uri();?>/js/affix.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/alert.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/button.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/carousel.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/collapse.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/dropdown.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/modal.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/popover.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/scrollspy.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/site.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/tab.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/tooltip.js"></script>
     <script src="<?php echo get_template_directory_uri();?>/js/transition.js"></script>
 

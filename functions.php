6d4e14affb590d66f8d9a9149de27ffebdb76aea
igloughlen <igloughlen@gmail.com>
2014-03-28 02:30:51 -0700

    functions html5

--- functions.php
+++ functions.php
@@ -41,6 +41,16 @@
 		'footer' => __( 'Footer menu', 'frankenstarkers' ),
 	) );
 
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+
+
 	/* ========================================================================================================================
 	
 	Actions and Filters
